f355bf208efee963ec87c6d6778de7588b9a9bb1
marco <marco@DESKTOP-POIBB7K>
2025-06-30 20:59:10 -0400

    Add project files.

---
+++ WritingFunctions/Program.cs
@@ -0,0 +1,10 @@
+﻿// When MultiplyTable is called, number and size are arguments.
+MultiplyTable(number: 3, size: 5);
+
+// When CalculateTax is called, amount and twoLetterRegionCode are arguments.
+decimal tax = CalculateTax(amount: 100, twoLetterRegionCode: "CH");
+WriteLine($"The tax is {tax:C}"); // The :C format specifier formats the value as currency.
+
+RunCardinalToOrdinal();
+
+RunFactorial();
